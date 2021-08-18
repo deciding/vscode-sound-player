83b1212b5927d5d621f27913d519b95b5c11bdd2
deciding <zhangzn710@gmail.com>
2021-08-18 12:18:58 +0800

    // EditorProvider, main class

--- src/SoundPlayerProvider.ts
+++ src/SoundPlayerProvider.ts
@@ -1,26 +1,27 @@
 import { CustomReadonlyEditorProvider, ExtensionContext, Uri, WebviewPanel } from 'vscode'
-import { SoundPlayerDocument } from './SoundPlayerDocument'
-import { GetWebviewContent } from './Util'
+import { SoundPlayerDocument } from './SoundPlayerDocument' // 
+import { GetWebviewContent } from './Util' // 
 
-export class SoundPlayerProvider implements CustomReadonlyEditorProvider<SoundPlayerDocument> {
+export class SoundPlayerProvider implements CustomReadonlyEditorProvider<SoundPlayerDocument> { // readonly editor
 
-    private html: Promise<string>
+    private html: Promise<string> // webview content
 
     constructor(context: ExtensionContext) {
-        this.html = GetWebviewContent(context.asAbsolutePath('public'))
+        // GetWebviewContent is from Util, replace script/css to actual filepath
+        this.html = GetWebviewContent(context.asAbsolutePath('public')) // join(context.extensionUri, 'public')
     }
 
-    openCustomDocument(uri: Uri): SoundPlayerDocument {
+    openCustomDocument(uri: Uri): SoundPlayerDocument { // create doc and pass to resolveCustomEditor for viewing
         return new SoundPlayerDocument(uri)
     }
 
     async resolveCustomEditor(document: SoundPlayerDocument, webviewPanel: WebviewPanel): Promise<void> {
         webviewPanel.webview.options = {
-            enableScripts: true
+            enableScripts: true // enable webview to run scripts
         }
         const html = await this.html
-        webviewPanel.webview.html = html
+        webviewPanel.webview.html = html // webview static content
         const result = await document.parseResult
-        webviewPanel.webview.postMessage(result)
+        webviewPanel.webview.postMessage(result) // post the message to the webview, for dynamic content
     }
 }
