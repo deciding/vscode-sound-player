ba556d61f89b33211a835d3c7f8bd80f04258a09
deciding <zhangzn710@gmail.com>
2021-08-18 12:00:07 +0800

    // EditorProvider registration.
    Activation is when onCustomEditor:asurance.soundplayer fired

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,15 @@
-import { ExtensionContext, window } from 'vscode'
-import { SoundPlayerProvider } from './SoundPlayerProvider'
+import { ExtensionContext, window } from 'vscode' // builtin import
+import { SoundPlayerProvider } from './SoundPlayerProvider' // main class
 
-export function activate(context: ExtensionContext): void {
+export function activate(context: ExtensionContext): void { // activate function activated by the onCustomEditor:asurance.soundplayer event
+    // In short, all disposable objects that your extension creates should go into subscriptions in some way so that they can be properly cleaned up 
+    //when your extension is unloaded.
+    // https://stackoverflow.com/a/55579618/8470034
+    // viewtype: asurance.soundplayer
     context.subscriptions.push(window.registerCustomEditorProvider('asurance.soundplayer', new SoundPlayerProvider(context), {
         webviewOptions: {
-            retainContextWhenHidden: true
+            retainContextWhenHidden: true // keep the state when hide the webview
         },
-        supportsMultipleEditorsPerDocument: true,
+        supportsMultipleEditorsPerDocument: true, // allw multipe editors available
     }))
 }
